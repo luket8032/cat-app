1626ed9e7fe29e4c091bd20f323858f679f5770a
luket8032 <luket8032@hotmail.com>
2023-05-02 15:04:58 -0500

    added other pages and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import News from './components/News.jsx';
+import Games from './components/Games.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+          <Navbar/>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/news' element={<News/>}/>
+              <Route path='/games' element={<Games/>}/>
+            </Routes>
+          </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import catlogo from '../assets/mmocat.png'
 import '../App.css';
 
 const Navbar = () => {
+  
   return (
     <div class='nav'>
       <div class='logo'>
-      <img class='mmocat-icon' src={catlogo} alt="catlogo"/>
-      <h1 class='logo-text'>MMOCat</h1>
-      </div>
-
-      <div>
-        <ul className='flex'>
-
+        <img class='mmocat' src={catlogo} alt="catlogo"/>
+      
+        <ul class='navlist'>
+          <li><a href="/" className='navlink'>Home</a></li>
+          <li><a href="/news" className='navlink'>News</a></li>
+          <li><a href="/games" className='navlink'>Games</a></li>
         </ul>
       </div>
     </div>
