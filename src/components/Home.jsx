26bfb19fcb123e296339f09f9cec46b08526b896
luket8032 <luket8032@hotmail.com>
2023-05-03 15:12:31 -0500

    got key to work with thecatapi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState }  from 'react'
 import '../assets/css/home.css'
 
 const Home = () => {
+  const api_key = 'live_VCzktalCGYDHVGnpidpr17uDiRrkOePItc8ABgKWRzqm5jxR0QjpuUbrKwBtFCeM'
   const [results, setResults] = useState([])
 
   useEffect(() => {
-    fetch('https://api.thecatapi.com/v1/images/search')
+    fetch('https://api.thecatapi.com/v1/images/search?limit=4&breed_ids=beng', {headers: {'x-api-key': api_key}})
     .then(res => res.json())
-    .then(data => setResults(data));
+    .then(data => setResults(data))
   }, [])
 
   return (
