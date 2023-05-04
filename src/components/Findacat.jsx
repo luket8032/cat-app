feeb84443bac6a45e7a9f9249bdb7e6abd69e291
luket8032 <66449324+luket8032@users.noreply.github.com>
2023-05-04 14:07:42 -0500

    ADDED SPINNERS

--- src/components/Findacat.jsx
+++ src/components/Findacat.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState }  from 'react'
 import '../assets/css/findacat.css'
+import BeatLoader from "react-spinners/BeatLoader";
 
 const Findacat = () => {
   const api_key = 'live_VCzktalCGYDHVGnpidpr17uDiRrkOePItc8ABgKWRzqm5jxR0QjpuUbrKwBtFCeM';
   const [results, setResults] = useState([])
+  const [isLoading, setLoading] = useState(false);
+  const [order, setOrder] = useState('');
+  const [page, setPage] = useState('')
+  const [breed, setBreed] = useState('')
 
   useEffect(() => {
-    fetch('https://api.thecatapi.com/v1/images/search?limit=24', {headers: {'x-api-key': api_key}})
+    setLoading(true);
+    fetch('https://api.thecatapi.com/v1/images/search?order=RANDOM&page=&limit=25', {headers: {'x-api-key': api_key}})
     .then(res => res.json())
     .then(data => setResults(data))
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally (() => {
+      setLoading(false);
+    })
   }, [])
+
+  if (isLoading) {
+    return (
+      <>
+      <div className='findacat-loader'>
+      <BeatLoader color='grey'/>
+      </div>
+      </>
+    )
+
+  }
 
   return (
     <div className='image-grid'>
