3e568f4d39545520ea6b81dca7a09bad677124e0
luket8032 <luket8032@hotmail.com>
2023-05-08 15:48:21 -0500

    modal position fixed
    still need to resize images

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../assets/css/findacat.css'
 
 const Modal = ({ showModal, setShowModal, selectedImage }) => {
+    const modalRef = useRef();
+
+    const closeModal = e => {
+        if(modalRef.current === e.target) {
+            setShowModal(false);
+        }
+    }
   return (
     <>
     {showModal ? 
-    <div className='background'>
+    <div className='background' ref={modalRef} onClick={closeModal}>
         <div className='modal-wrapper'>
             <img src={selectedImage} alt='selected'></img>
         </div>
